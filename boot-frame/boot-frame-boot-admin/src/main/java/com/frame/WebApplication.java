d27cb32047f9572902771d664557591cf670428c
duanc <changqing.duan@163.com>
2016-12-20 21:34:38 +0800

    11

--- boot-frame/boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
+++ boot-frame/boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
@@ -3,7 +3,7 @@ package com.frame;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 

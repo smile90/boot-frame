09810e67881cdfa11d9194f657a7e9d3c1bad253
duanc <changqing.duan@163.com>
2016-12-20 18:12:28 +0800

    11

--- boot-frame/boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
+++ boot-frame/boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
@@ -3,7 +3,6 @@ package com.frame;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,11 +12,6 @@ import org.springframework.context.annotation.PropertySource;
 @EnableAdminServer
 @PropertySource("classpath:/application.properties")
 public class WebApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);

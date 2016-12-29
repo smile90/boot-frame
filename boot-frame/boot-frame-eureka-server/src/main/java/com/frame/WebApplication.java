321c759952b5472ddbf2dd3e90e7b8089ce6a5d7
duanc <changqing.duan@163.com>
2016-12-29 11:02:48 +0800

    11

--- boot-frame/boot-frame-eureka-server/src/main/java/com/frame/WebApplication.java
+++ boot-frame/boot-frame-eureka-server/src/main/java/com/frame/WebApplication.java
@@ -1,8 +1,22 @@
 package com.frame;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
+
 @SpringBootApplication
 @EnableEurekaServer
 @EnableConfigurationProperties
+@PropertySource(
+    ignoreResourceNotFound = true,
+    value = {
+        "classpath:/application.properties",
+        "classpath:/config/eureka.properties"
+    }
+)
 public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {

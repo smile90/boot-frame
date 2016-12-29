b94948791944c630355dd83b7d6ba66c5c70694a
duanc <changqing.duan@163.com>
2016-12-29 09:59:00 +0800

    11

--- boot-frame/boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
+++ boot-frame/boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
@@ -5,17 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableAdminServer
 @EnableConfigurationProperties
-@PropertySource(
-        ignoreResourceNotFound = true,
-        value = {
-                "classpath:/application.properties"
-        }
-)
 public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {

88df565ae1f22edac35d7a4ae433ddf525293634
duanc <changqing.duan@163.com>
2017-06-01 19:27:08 +0800

    11

--- boot-frame/boot-frame-portal/src/main/java/com/frame/boot/frame/WebApplication.java
+++ boot-frame/boot-frame-portal/src/main/java/com/frame/boot/frame/WebApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableDiscoveryClient
-@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"com.frame"})
 @ServletComponentScan(basePackages = {"com.frame"})
 @EnableConfigurationProperties

6d85aa71adff422b3e1cfa9d6705fcf7929647fc
duanchangqing90@163.com <woai7227440>
2017-06-14 21:41:06 +0800

    1

--- boot-frame/boot-frame-security/src/main/resources/static/lib/ui/admin/js/login.js
+++ boot-frame/boot-frame-security/src/main/resources/static/lib/ui/admin/js/login.js
@@ -2,8 +2,13 @@ $(function() {
     // 注册登录方法
     $("#loginForm").ajaxForm({
         success: function(data, statusText) {
-            $("#showMsg").text(data.showMsg);
-            return false;
+            var result = data.result;
+            if ("SUCCESS" != result) {
+                $("#showMsg").text(data.showMsg);
+                return false;
+            } else {
+                return true;
+            }
         },
         error: function (data) {
             $("#showMsg").text(data.showMsg);

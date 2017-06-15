1a390370697acba04069a1272de0c996086def72
duanchangqing90@163.com <woai7227440>
2017-06-15 10:15:54 +0800

    1

--- boot-frame/boot-frame-security/src/main/java/com/frame/boot/frame/WebApplication.java
+++ boot-frame/boot-frame-security/src/main/java/com/frame/boot/frame/WebApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@RefreshScope
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableEurekaClient
 public class WebApplication {
 
     public static void main(String[] args) {

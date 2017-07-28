b17621d18d5504352f6f201ad4f5a4179bca3456
duanchangqing90@163.com <woai7227440>
2017-07-28 16:43:38 +0800

    1

--- boot-frame/boot-frame-security/src/main/resources/static/lib/ui/admin/js/index.js
+++ boot-frame/boot-frame-security/src/main/resources/static/lib/ui/admin/js/index.js
@@ -9,6 +9,17 @@ $(function () {
         dataType: "JSON",
         success: function (data, textStatus) {
             // 数据、请求状态
+            $.each(data, function (index, domEle) {
+                if (domEle.parentCode == null || domEle.parentCode == "") {
+                    $("#menu-show").append(
+                        "<dl id=\"" + domEle.code + "\">" +
+                        "<dt><i class=\"Hui-iconfont\">" + domEle.icon + "</i> " + domEle.name + "<i class=\"Hui-iconfont menu_dropdown-arrow\">&#xe6d5;</i></dt>" +
+                        "<dd><ul></ul></dd>" +
+                        "</dl>");
+                } else {
+                    $("#" + domEle.parentCode + " dt dd ul").append("<li><a data-href=\"" + domEle.url + "\" data-title=\"" + domEle.name + "\" href=\"javascript:void(0)\">" + domEle.name + "</a></li>");
+                }
+            });
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             layer.alert("[" + XMLHttpRequest.status + "][" + textStatus + "][" + errorThrown + "]", {icon: 2});

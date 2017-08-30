b001fd59295c26d37dfb8a289ba5332d238a81d9
duanchangqing90@163.com <woai7227440>
2017-08-30 11:35:34 +0800

    1

--- boot-frame/boot-frame-cfg-client-test/src/main/java/com/frame/boot/frame/WebApplication.java
+++ boot-frame/boot-frame-cfg-client-test/src/main/java/com/frame/boot/frame/WebApplication.java
@@ -2,8 +2,10 @@ package com.frame.boot.frame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 public class WebApplication

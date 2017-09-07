c9afc180236d525a52abd654486124747d530a2b
duanchangqing90@163.com <woai7227440>
2017-09-07 15:30:14 +0800

    1

--- boot-frame/boot-frame-core/src/main/java/com/frame/boot/frame/core/entity/IDGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.frame.boot.frame.core.entity;
-
-
-import org.hibernate.id.UUIDGenerator;
-
-public class IDGenerator extends UUIDGenerator {
-
-}
--- boot-frame/boot-frame-portal/src/main/java/com/frame/boot/frame/portal/constants/RemoteServiceConstant.java
+++
@@ -1,9 +0,0 @@
-package com.frame.boot.frame.portal.constants;
-
-public class RemoteServiceConstant {
-
-    private RemoteServiceConstant() {}
-
-    public static final String SECURITY_SERVER = "BOOT-FRAME-SECURITY";
-    public static final String SECURITY_SERVICE_FIND_USER = "http://" + SECURITY_SERVER + "/security/user";
-}

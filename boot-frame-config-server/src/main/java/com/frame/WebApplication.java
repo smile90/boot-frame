a9878a3a9555c18e40b0976b9227a50dbcf0aea5
duanchangqing90@163.com <woai7227440>
2017-09-13 15:22:25 +0800

    1

--- boot-frame-config-server/src/main/java/com/frame/WebApplication.java
+++ boot-frame-config-server/src/main/java/com/frame/WebApplication.java
@@ -2,6 +2,7 @@ package com.frame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -11,6 +12,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableConfigServer
 @EnableEurekaClient
 public class WebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
--- boot-frame-eureka-server/src/main/java/com/frame/WebApplication.java
+++ boot-frame-eureka-server/src/main/java/com/frame/WebApplication.java
@@ -2,12 +2,18 @@ package com.frame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);

d793541d5abe2330907d0469147fbf078d6ae2ac
duanchangqing90@163.com <woai7227440>
2017-09-30 13:56:49 +0800

    1

--- boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
+++ boot-frame-boot-admin/src/main/java/com/frame/WebApplication.java
@@ -1,17 +1,55 @@
 package com.frame;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.notify.LoggingNotifier;
+import de.codecentric.boot.admin.notify.Notifier;
+import de.codecentric.boot.admin.notify.RemindingNotifier;
+import de.codecentric.boot.admin.notify.filter.FilteringNotifier;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
 
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableAutoConfiguration
 public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
+    }
+
+    @Configuration
+    public static class NotifierConfig {
+        @Bean
+        @Primary
+        public RemindingNotifier remindingNotifier() {
+            RemindingNotifier notifier = new RemindingNotifier(filteringNotifier(loggerNotifier()));
+            notifier.setReminderPeriod(TimeUnit.SECONDS.toMillis(10));
+            return notifier;
+        }
+
+        @Scheduled(fixedRate = 1_000L)
+        public void remind() {
+            remindingNotifier().sendReminders();
+        }
+
+        @Bean
+        public FilteringNotifier filteringNotifier(Notifier delegate) {
+            return new FilteringNotifier(delegate);
+        }
+
+        @Bean
+        public LoggingNotifier loggerNotifier() {
+            return new LoggingNotifier();
+        }
     }
 }

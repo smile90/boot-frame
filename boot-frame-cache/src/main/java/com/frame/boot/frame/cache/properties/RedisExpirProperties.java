432be6140dc778d22ce6dfeb063e0f5df7233ace
duanchangqing90@163.com <woai7227440>
2017-11-16 15:06:04 +0800

    1

--- boot-frame-cache/src/main/java/com/frame/boot/frame/cache/properties/RedisExpirProperties.java
+++ boot-frame-cache/src/main/java/com/frame/boot/frame/cache/properties/RedisExpirProperties.java
@@ -3,13 +3,11 @@ package com.frame.boot.frame.cache.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@PropertySource(value = {"classpath:/config/redisExpir.properties"}, ignoreResourceNotFound = true)
 @ConfigurationProperties(prefix = "redis.expir", ignoreInvalidFields = true)
 public class RedisExpirProperties {
 	

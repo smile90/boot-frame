714d23fba4b305e3e67a8fdad63bf3ea352ed6d4
duanchangqing90@163.com <woai7227440>
2017-12-11 16:01:19 +0800

    1

--- boot-frame-config-server/src/main/java/com/frame/WebApplication.java
+++ boot-frame-config-server/src/main/java/com/frame/WebApplication.java
@@ -1,19 +1,12 @@
 package com.frame;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringCloudApplication
-public class WebApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WebApplication.class);
-    }
+public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
--- boot-frame-eureka-server/src/main/java/com/frame/WebApplication.java
+++ boot-frame-eureka-server/src/main/java/com/frame/WebApplication.java
@@ -1,19 +1,12 @@
 package com.frame;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringCloudApplication
-public class WebApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WebApplication.class);
-    }
+public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);

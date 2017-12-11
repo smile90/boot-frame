eb48fd98158da8610c6f87e7272968929fa36849
duanchangqing90@163.com <woai7227440>
2017-12-11 16:04:07 +0800

    1

--- boot-frame-security/src/main/java/com/frame/boot/frame/WebApplication.java
+++ boot-frame-security/src/main/java/com/frame/boot/frame/WebApplication.java
@@ -1,17 +1,10 @@
 package com.frame.boot.frame;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
-public class WebApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(WebApplication.class);
-    }
+public class WebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);

fd87c7927f3601fcc5374c1bdc01ea36ffaf0e7e
duanchangqing90@163.com <woai7227440>
2018-01-04 21:05:46 +0800

    1

--- boot-frame-security/src/main/java/com/frame/boot/frame/security/service/SysUserService.java
+++ boot-frame-security/src/main/java/com/frame/boot/frame/security/service/SysUserService.java
@@ -4,12 +4,20 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.frame.boot.frame.security.entity.SysUser;
 import com.frame.boot.frame.security.mapper.SysUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service("sysUserService")
-public class SysUserService extends ServiceImpl<SysUserMapper, SysUser> {
+public class SysUserService extends ServiceImpl<SysUserMapper, SysUser> implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return findSecurityUserByUsername(username);
+    }
 
     public SysUser create(SysUser sysUser) {
         sysUser.setCreateTime(new Date());
@@ -42,4 +50,5 @@ public class SysUserService extends ServiceImpl<SysUserMapper, SysUser> {
     public SysUser findSecurityUserByUsername(String username) {
         return baseMapper.selectSecurityUserByUsername(username);
     }
+
 }

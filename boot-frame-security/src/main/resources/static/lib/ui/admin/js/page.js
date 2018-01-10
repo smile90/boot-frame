e1a452cf8372904f90a6e335824c00f5222abbcd
duanchangqing90@163.com <woai7227440>
2018-01-10 18:53:44 +0800

    1

--- boot-frame-security/src/main/resources/static/lib/ui/admin/js/page.js
+++ boot-frame-security/src/main/resources/static/lib/ui/admin/js/page.js
@@ -1,15 +1,17 @@
 PageTable = {
-    responseHandler: function(res) {
+    responseHandler: function (res) {
         return {
             "total": res.total,  //总页数
             "rows": res.records   //数据
         };
     },
-    queryParams: function(params) {
-        console.info(params);
-        return {
-            "page.offset": params.offset,
-            "page.offset": params.offset
-        };
+    queryParams: function (params) {
+        var current = (params.offset == 0 ? 1 : (params.offset / params.limit + 1));
+        return $.extend(params, {
+            "current": current,
+            "size": params.limit,
+            "orderByField": params.sort,
+            "asc": (params.order == "desc" ? false : true)
+        });
     }
 };

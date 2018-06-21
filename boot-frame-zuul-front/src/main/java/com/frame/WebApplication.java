8678b80dfd9a6067a50880b2fe75cd39294220ee
duanchangqing90@163.com <woai7227440>
2018-06-21 16:11:53 +0800

    1

--- boot-frame-zuul-front/src/main/java/com/frame/WebApplication.java
+++ boot-frame-zuul-front/src/main/java/com/frame/WebApplication.java
@@ -1,11 +1,11 @@
 package com.frame;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@SpringCloudApplication
 public class WebApplication {
 
     public static void main(String[] args) {

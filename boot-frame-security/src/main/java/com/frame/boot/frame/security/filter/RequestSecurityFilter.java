39e0559fa8d830f8f7f73efad0595e1f1aebf9bf
duanchangqing90@163.com <woai7227440>
2018-07-06 11:03:22 +0800

    1

--- boot-frame-security/src/main/java/com/frame/boot/frame/security/filter/RequestSecurityFilter.java
+++ boot-frame-security/src/main/java/com/frame/boot/frame/security/filter/RequestSecurityFilter.java
@@ -2,15 +2,13 @@ package com.frame.boot.frame.security.filter;
 
 
 import com.alibaba.fastjson.JSONObject;
-import com.frame.boot.frame.security.entity.SysUser;
+import com.frame.boot.frame.security.utils.AuthUtil;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -36,17 +34,15 @@ public class RequestSecurityFilter extends ZuulFilter {
     @Override
     public Object run() throws ZuulException {
         try {
-            RequestContext context = RequestContext.getCurrentContext();
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             JSONObject bossAuth = new JSONObject();
-            if (auth != null) {
-//                if (auth.getPrincipal() instanceof  SysUser) {
+            bossAuth.put("username", AuthUtil.getUsername());
+//            Authentication user = AuthUtil.getUser();
+//            if (user != null) {
+//                if (user.getPrincipal() instanceof  SysUser) {
 //                    SysUser user = (SysUser) auth.getPrincipal();
-//                    bossAuth.put("username", user.getUsername());
 //                }
-                bossAuth.put("username", auth.getName());
-                context.addZuulRequestHeader("bossAuth", bossAuth.toJSONString());
-            }
+//            }
+            RequestContext.getCurrentContext().addZuulRequestHeader("bossAuth", bossAuth.toJSONString());
         } catch (Exception e) {
             logger.error("set bossAuth is error.", e);
         }

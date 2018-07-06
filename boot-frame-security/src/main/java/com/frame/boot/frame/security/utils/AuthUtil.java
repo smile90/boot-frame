629ba997ac27784d6708e96c2160dbb1b6a82e0d
duanchangqing90@163.com <woai7227440>
2018-07-06 11:04:06 +0800

    1

--- boot-frame-security/src/main/java/com/frame/boot/frame/security/utils/AuthUtil.java
+++ boot-frame-security/src/main/java/com/frame/boot/frame/security/utils/AuthUtil.java
@@ -6,8 +6,16 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuthUtil {
 
+    public static Authentication getUser() {
+        if (SecurityContextHolder.getContext() != null) {
+            return SecurityContextHolder.getContext().getAuthentication();
+        } else {
+            return null;
+        }
+    }
+
     public static String getUsername() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Authentication auth = getUser();
         if (auth != null) {
             SysUser user = (SysUser) auth.getPrincipal();
             return user.getUsername();
